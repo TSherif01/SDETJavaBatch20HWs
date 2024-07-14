82a0a6ada6d9cd7f6d6bdcfc583adfc955c3a3b3
TSherif01 <Tesneemsherif@gmail.com>
2024-07-14 12:40:07 -0400

    first hw done.

--- src/main/java/org/example/e28/E28SimpleCalculatorElseIf.java
+++ src/main/java/org/example/e28/E28SimpleCalculatorElseIf.java
@@ -3,13 +3,24 @@ package org.example.e28;
 public class E28SimpleCalculatorElseIf {
     public static void main(String[] args) {
         // Declare a variable for operator and assign a value
-        String operator = "+";
-
-
+        String operator = "+" + "-" + "*" + "/";
+        operator = "*";
         // Declare two variables for numbers and assign values
-
+        int num1 = 10;
+        int num2 = 5;
 
         // Perform the corresponding arithmetic operation
-
+        if (operator.equals("+")) {
+            System.out.println("The sum is: " + (num1 + num2));
+        } else if (operator.equals("-")) {
+            System.out.println("The difference is: " + (num1 - num2));
+        } else if (operator.equals("*")) {
+            System.out.println("The product is: " + (num1 * num2));
+        } else if (operator.equals("/")) {
+            System.out.println("The quotient is: " + (num1 / num2));
+        } else {
+            System.out.println("Invalid operator");
+        }
     }
 }
+
--- src/main/java/org/example/e29/E29ClassifyAnimalsElseIf.java
+++ src/main/java/org/example/e29/E29ClassifyAnimalsElseIf.java
@@ -3,9 +3,21 @@ package org.example.e29;
 public class E29ClassifyAnimalsElseIf {
     public static void main(String[] args) {
         // Declare a string variable and assign a value
-
+        String animal = "dog";
 
         // Use if-else-if conditions to classify the animal
-
+        if (animal.equals("dog")) {
+            System.out.println("The animal is a mammal.");
+        }else if (animal.equals("eagle")) {
+            System.out.println("The animal is a bird.");
+        }else if (animal.equals("shark")) {
+            System.out.println("The animal is a fish.");
+        }else if (animal.equals("frog")) {
+            System.out.println("The animal is a amphibian.");
+        }else if (animal.equals("snake")) {
+            System.out.println("The animal is a reptile.");
+        }else{
+            System.out.println("Unknown animal type.");
+        }
     }
 }
